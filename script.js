bca5fcae86f2074b4bcd5241ebbac9f25d92cd53
Adarshpaulraj <smartadarsh832@gmail.com>
2023-08-05 11:30:06 +0530

    code updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1.Print odd numbers in an array
 const odd = (arr) => {
-  result = [];
+ let result = [];
   for (var i=0;i<arr.length;i++){
        if(arr[i]%2!==0){
         result.push(arr[i]);
@@ -14,14 +14,30 @@ const odd = (arr) => {
  
   // 2.Convert all the strings to title caps in a string array 
    
-function title(str) {
+function title (str) {
+  str = str.toLowerCase().split(" ");
   
   
-  str = str.toLowerCase().split(' ');
   for (let i = 0; i < str.length; i++) {
+    
       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
-  return str.join(' ');
+  return str.join(" ");
 }
 console.log(title("geeks for geeks"));
 console.log(title("fuction task"));
+
+// Sum of all numbers in an array
+ 
+var arr = [1,2,3,4,5,6,]
+const output= arr.reduce(function(max,curr){
+  if(curr>max){
+    max= curr;
+  }return max;
+},0)
+console.log(output);
+ 
+
+
+ 
+ 
